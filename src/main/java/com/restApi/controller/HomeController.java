6e1bf2c5cba8c8c2f199092084ece6ec5f023856
EunKyoung An <111840622+Aneunkyoung@users.noreply.github.com>
2022-09-02 13:17:17 +0900

    Update HomeController.java

--- src/main/java/com/restApi/controller/HomeController.java
+++ src/main/java/com/restApi/controller/HomeController.java
@@ -1,21 +1,13 @@
 package com.restApi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.restApi.service.BoardService;
-
 @Controller
 public class HomeController {
-	@Autowired
-	BoardService boardService;
-	
 	@RequestMapping(value = "/board", method = RequestMethod.GET)
-	public String getAllBoard(Model model){
-		model.addAttribute("commentCount", boardService.getBoardCount());
+	public String getAllBoard(){
 		return "board/board"; 
 	}  
 }
